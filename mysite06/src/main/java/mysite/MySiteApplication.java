e747cfffdb1c03dad12ec0079564f582c3cb9199
DAY0522 <ekdud_____@naver.com>
2025-01-21 17:12:09 +0900

    mysite06

---
+++ mysite06/src/main/java/mysite/MySiteApplication.java
@@ -0,0 +1,15 @@
+package mysite;
+
+import mysite.config.AppConfig;
+import mysite.config.WebConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+@SpringBootApplication
+@Import({AppConfig.class, WebConfig.class})
+public class MySiteApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MySiteApplication.class, args);
+    }
+}
